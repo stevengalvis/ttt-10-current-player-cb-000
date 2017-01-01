1c689eb10fd99093d58fc7fa96e6a2a4511279b8
Steven Galvis <stevengalvis@ymail.com>
2017-01-01 18:16:25 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,26 @@
+def turn_count(board)
+    counter = 0
+board.each do |position|
+
+  if position == "X" || position == "O"
+    counter += 1
+
+  end
+    end
+
+    return counter
+end
+
+
+def current_player(board)
+
+  move_count = turn_count(board)
+
+  if move_count % 2 == 0
+    return "X"
+
+  else
+    return "O"
+
+  end
+end
